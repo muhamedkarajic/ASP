ace4e1c4a1bd44239d6f1f675429e932931b57ec
Muhamed Karajic <35216938+muhamedkarajic@users.noreply.github.com>
2018-10-16 16:41:08 +0200

    Update ciklicniNiz

---
+++ ciklicniNiz.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+using namespace std;
+
+class ciklicniNiz
+{
+	int _pocetak = 0;
+	int _maxSize = 5;
+	int _kraj = 0;
+	int* _niz = new int[_maxSize];
+	bool pun = false;
+
+	int getSljedeci(const int &lokacija) const {
+		if (lokacija + 1 == _maxSize)
+			return 0;
+		return lokacija + 1;
+	}
+
+
+public:
+	bool jelPun() { return _kraj == _pocetak; }
+	
+	void Ukloni()
+	{
+		_pocetak = getSljedeci(_pocetak);
+		if (pun)
+			pun = false;
+	}
+
+	void Dodaj(const int &elemenet) {
+
+		if (!pun) {
+			_niz[_kraj] = elemenet;
+			_kraj = getSljedeci(_kraj);
+			if (jelPun())
+				pun = true;
+		}
+		else
+			cout << "Niz je pun." << endl;
+	}
+	
+	void Ispis()
+	{
+		do
+		{
+			cout << _niz[_pocetak] << ", ";
+			_pocetak = getSljedeci(_pocetak);
+		} while (_pocetak != _kraj);
+	}
+	
+};
+
+int main()
+{
+	ciklicniNiz niz;
+	niz.Dodaj(5);
+	niz.Dodaj(4);
+	niz.Dodaj(3);
+	niz.Dodaj(2);
+	niz.Dodaj(1);
+	niz.Ukloni();
+	niz.Dodaj(50);
+	niz.Ukloni();
+	niz.Dodaj(40);
+	niz.Ukloni();
+	niz.Dodaj(30);
+
+	niz.Ispis();
+
+	system("PAUSE>0");
+}
