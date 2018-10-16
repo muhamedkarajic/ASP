0acc69aeebbb38afcb214dedc0d9ec0f29a76f0b
Muhamed Karajic <35216938+muhamedkarajic@users.noreply.github.com>
2018-10-16 22:23:02 +0200

    Update quickSort

--- quickSort.cpp
+++ quickSort.cpp
@@ -1,47 +1,41 @@
 ﻿#include <iostream>
 using namespace std;
 
-//learned using geeksforgeeks
-
-int partition(int arr[], int low, int high)
-{
+int partition(int *arr, const int &low, const int &high) {
+	int i = low - 1;
 	int pivot = arr[high];
-	int i = (low - 1);
 
 	for (int j = low; j < high; j++)
-	{
 		if (arr[j] <= pivot)
-		{
-			i++;
-			swap(arr[i], arr[j]);
-		}
-	}
+			swap(arr[j], arr[++i]);
 
-	swap(arr[i + 1], arr[high]);
+	swap(arr[i+1], arr[high]);
+
 	return (i + 1);
 }
 
-void quickSort(int arr[], int low, int high)
-{
-	if (low < high)
-	{
-		int pi = partition(arr, low, high);
+void quickSort(int *arr, const int &low, const int &high) {
+	if (low < high) {
+		int middle = partition(arr, low, high);
 
-		quickSort(arr, low, pi - 1);
-		quickSort(arr, pi + 1, high);
+		quickSort(arr, low, middle - 1);
+		quickSort(arr, middle +1, high);
 	}
 }
 
-int main()
-{
-	int arr[] = { 4,2,5,1,8,9,7,3,6 };
-	int n = sizeof(arr) / sizeof(arr[0]);
-	quickSort(arr, 0, n - 1);
-
-
+void Print(int *arr, const int &n) {
 	for (int i = 0; i < n; i++)
 		cout << arr[i] << ", ";
 	cout << endl;
+}
+
+
+int main() {
+	int arr[] = { 6,23,1,56,2,7,2 };
+	int n = sizeof(arr) / sizeof(arr[0]);
+
+	quickSort(arr, 0, n - 1);
+	Print(arr, n);
 
 	system("PAUSE>0");
 }
