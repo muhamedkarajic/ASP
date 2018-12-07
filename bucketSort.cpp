aefb50f17af10a2bd0af0498da437c91b82aa10b
Muhamed Karajic <35216938+muhamedkarajic@users.noreply.github.com>
2018-12-07 17:44:43 +0100

    Update bucketSort

---
+++ bucketSort.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+void BucketSort(int A[], int n)
+{
+	PrRedPov *X = new PrRedPov[n];
+
+	int maxV = GetMaxV(A,n);
+
+	for(int i = 0; i<n; i++)
+	{
+		int value = A[i];
+		int p = value*n / (maxV+1);
+		X[p].Dodaj(value);
+	} 
+
+	int brojac = 0;//n-1
+	for(int i = n-1; i>=0; i--)
+	{
+		int x = X[i].Ukloni();
+		A[brojac] = x;
+		brojac++;
+	}
+}
